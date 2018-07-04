322d0037fc0c740bdc6a0726cb0ed4e3b6dfc35a
Med-94 <38832580+Mohamed-94@users.noreply.github.com>
2018-07-04 12:30:22 +0100

    Add files via upload

---
+++ BesierClock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Mohamed.Programming.CSharp.Clocks.BezierClock;
+using System.Drawing;
+using System.Windows.Forms;
+
+class BezierClock : Form
+{
+    BezierClockControl bclok;
+
+    //public static void Main()
+    //{
+    //    Application.Run(new BezierClock());
+    //}
+
+    public BezierClock()
+    {
+        Text = "BezierClock";
+
+        bclok = new BezierClockControl();
+        bclok.Parent = this;
+        bclok.Time = DateTime.Now;
+        bclok.Dock = DockStyle.Fill;
+        bclok.BackColor = Color.Black;
+        bclok.ForeColor = Color.White;
+
+        Timer timer = new Timer();
+        timer.Interval = 1000;
+        timer.Tick += new EventHandler(TimerOnTick);
+        timer.Start();
+
+    }
+    void TimerOnTick(object obj, EventArgs ea)
+    {
+        bclok.Time = DateTime.Now;
+    }
+}
